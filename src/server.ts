8ca9018c29279935fef5ef2c21b6038f5fc4ffd3
GimbeiNa89 <brunoherculessilva@gmail.com>
2024-04-08 16:53:08 +0200

    server e database funzionanti, cambio da app a server come punto di starting

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 import app from "./app";
+const PORT = 3000;
 
 const CONNECTION_URL: string = "mongodb://localhost:27017";
-const PORT = 3000;
 
 mongoose
   .connect(CONNECTION_URL)
